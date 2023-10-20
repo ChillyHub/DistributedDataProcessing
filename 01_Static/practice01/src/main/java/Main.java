e22bc65d848cf58d3646730d7446bc9c2782d3ad
Ben Chilly <1533935729@qq.com>
2023-10-21 02:51:21 +0800

    Init commit

--- 01_Static/practice01/src/main/java/Main.java
+++ 01_Static/practice01/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        RunyuYan c1 = new RunyuYan();
-        RunyuYan c2 = new RunyuYan();
-        RunyuYan c3 = new RunyuYan();
+        new RunyuYan();
+        new RunyuYan();
+        new RunyuYan();
     }
 }
