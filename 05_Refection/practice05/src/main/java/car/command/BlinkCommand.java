521909228279ee3b647d71135dad39a664617d02
Ben Chilly <1533935729@qq.com>
2023-11-02 09:31:06 +0800

    commit

---
+++ 05_Refection/practice05/src/main/java/car/command/BlinkCommand.java
@@ -0,0 +1,39 @@
+package car.command;
+
+import car.Car;
+import car.util.ColorFactory;
+
+import java.awt.*;
+
+public class BlinkCommand implements Command{
+    private final Car car;
+    private final Color blinkColor;
+
+    public BlinkCommand(Car car, Color blinkColor){
+        this.car = car;
+        this.blinkColor = blinkColor;
+    }
+
+    @Override
+    public boolean execute() {
+        Color oldColor = car.getColor();
+        Color newColor = blinkColor;
+        for(int i = 0; i < 10; i++) {
+            car.setColor(newColor);
+            try {
+                Thread.sleep(car.getSpeed());
+                System.out.println("blink!!!");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            car.repaint();
+
+            Color tmp = oldColor;
+            oldColor = newColor;
+            newColor = tmp;
+        }
+        car.setColor(oldColor);
+        return true;
+    }
+}
